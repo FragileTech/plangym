83f122bd678a7c073703fd9d7ab30eaff015d4ac
guillemdb <guillem.db@gmail.com>
2020-03-25 16:21:26 +0100

    Fix bump version

--- plangym/__init__.py
+++ plangym/__init__.py
@@ -1,5 +1,10 @@
 """Various environments for plangym."""
-from plangym.dm_control import DMControlEnv, ParallelDMControl
-from plangym.env import AtariEnvironment, ParallelEnvironment
-from plangym.minimal import ClassicControl, MinimalPacman, MinimalPong
+import warnings
+
+try:
+    from plangym.dm_control import DMControlEnv, ParallelDMControl
+    from plangym.env import AtariEnvironment, ParallelEnvironment
+    from plangym.minimal import ClassicControl, MinimalPacman, MinimalPong
+except (ImportError, ModuleNotFoundError) as e:
+    warnings.warn("Failed to import module in plangym.__init__.py: %s" % str(e))
 from plangym.version import __version__
